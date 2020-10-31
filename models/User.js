ae3540af72c9ec412644c6c0c5d472d5c4ae8a30
Matthew O <mo@pop-os.localdomain>
2020-10-31 16:52:32 -0700

    added user model

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+// creates the user model class
+class User extends Model {}
+
+User.init(
+  // sets up columns
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+  },
+  // additional options
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "User",
+  }
+);
+
+module.exports = User;
