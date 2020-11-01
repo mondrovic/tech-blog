47c03b807ecc0f3b30be239ceffc6e30f8e6b145
Matthew O <mo@pop-os.localdomain>
2020-11-01 11:51:29 -0700

    added comment routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,2 +1,52 @@
 const router = require("express").Router();
 const { User, Post, Comment } = require("../../models");
+
+router.get("/", (req, res) => {
+  Comment.findAll()
+    .then((dbData) => res.json(dbData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post("/", (req, res) => {
+  /*
+  {
+      comment_body: "some comment text here",
+      user_id: 3,
+      post_id: 6
+  }
+  */
+  Comment.create({
+    comment_body: req.body.comment_body,
+    user_id: req.session.user_id,
+    post_id: req.body.post_id,
+  })
+    .then((dbData) => res.json(dbData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  Comment.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbData) => {
+      if (!dbData) {
+        res.status(404).json({ message: "No comment found with this id!" });
+        return;
+      }
+      res.json(dbData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
