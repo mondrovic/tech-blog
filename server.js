5eb53e974e27b6e427a52c20837197f38a447401
Matthew O <mo@pop-os.localdomain>
2020-11-01 17:17:57 -0700

    setup auth check and fixed handlebars issue

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const path = require("path");
 const express = require("express");
 const session = require("express-session");
-// const exphbs = require("express-handlebars");
-// const helpers = require("./utils/helper");
+const exphbs = require("express-handlebars");
+const helpers = require("./utils/helper");
 
 const app = express();
 const PORT = process.env.PORT || 3002;
@@ -22,10 +22,16 @@ const sess = {
 
 app.use(session(sess));
 
-// const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({
+  layoutsDir: path.join(__dirname, "/views/layouts"),
+  defaultLayout: "main.handlebars",
+  partialsDir: path.join(__dirname, "/views/partials"),
+  helpers: helpers,
+});
 
-// app.engine("handlebars", hbs.engine);
-// app.set("view engine", "handlebars");
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
